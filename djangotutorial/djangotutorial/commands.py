064649ccd41592052ba2f6bf4ffa01539e8dd2c1
Michael Davis <micdavis93@gmail.com>
2024-11-16 15:44:26 -0700

    practicing

---
+++ djangotutorial/djangotutorial/commands.py
@@ -0,0 +1,18 @@
+# ModelName.objects.get(parameter=value)
+# ModelName.objects.all(parameter=value)
+# ModelName.objects.filter(parameter=value)
+# ModelName.objects.any_custom_method(parameter1=value1, parameter2=value2)
+# ModelName.objects.
+# ModelName.objects.
+# ModelName.objects.
+# ModelName.objects.
+# ModelName.objects.
+# ModelName.objects.
+# ModelName.objects.
+# ModelName.objects.
+
+# .save() saves to database
+# .create() create and save
+
+# __startswith=value
+# __endswith=value
